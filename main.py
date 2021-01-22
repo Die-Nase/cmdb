667d24427fd4209ae38d4a84a843b9911dafb241
Die-Nase <l.lamparter@gmail.com>
2021-01-22 18:18:59 +0100

    added treeview to GUI

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
-
-
-class MyGrid(Widget):
-    pass
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.treeview import TreeView,TreeViewLabel
 
 
 class MyApp(App):
     def build(self):
-        return MyGrid()
+        return FloatLayout()
+
 
 if __name__ == "__main__":
     MyApp().run()
